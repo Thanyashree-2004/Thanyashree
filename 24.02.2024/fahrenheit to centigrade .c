7aa7b10161754654e9d2e3e514869a453b867037
Thanyashree-2004 <161038213+Thanyashree-2004@users.noreply.github.com>
2024-02-24 12:01:01 +0530

    Create fahrenheit to centigrade .c

---
+++ 24.02.2024/fahrenheit to centigrade .c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    float fahre,cent;
+    printf("enter the temperatur in fahrenheit");
+    scanf("%f",&fahre);
+    cent=(fahre-32)*5/9;
+    printf("the temperature in centigrade is:%f",cent);
+}
+o/p:
+enter the temperatur in fahrenheit100
+the temperature in centigrade is:37.777779
+
+
+
+
+  Formula:
+centigrade=(fahrenheit-32)*5/9
